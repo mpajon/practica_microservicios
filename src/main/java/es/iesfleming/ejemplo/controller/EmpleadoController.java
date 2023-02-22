52dd5fd315f3e1669beb4ff0669b788dd3c7db99
mpajon_Ricoh <marcos.pajon@ricoh.es>
2023-02-22 14:19:59 +0100

    Clase EmpleadoController

---
+++ src/main/java/es/iesfleming/ejemplo/controller/EmpleadoController.java
@@ -0,0 +1,11 @@
+package es.iesfleming.ejemplo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/empleado")
+@Slf4j
+public class EmpleadoController {
+
+}
